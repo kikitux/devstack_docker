4794eb9c40c91cfc9a0a1cb98df49def28146bc7
Jon Benson <bensojon@gmail.com>
2015-05-20 15:32:43 -0700

    Append each server block rather than overwriting

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -3,13 +3,13 @@ sudo apt-get install -y nginx
 [ -f /etc/nginx/sites-enabled/default ] && sudo rm /etc/nginx/sites-enabled/default
 
 for i in 1 2 3 4 5 ; do
-  cat <<EOF | sudo tee /etc/nginx/sites-enabled/demo >/dev/null
+  cat <<EOF | sudo tee -a /etc/nginx/sites-enabled/demo >/dev/null
 server {
         listen 888$i ;
         server_name localhost;
 
         location / {
-                proxy_pass http://10.0.0.$i:80/;    
+                proxy_pass http://10.0.0.$i:80/;
         }
 
 }
